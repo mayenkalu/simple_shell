472b0dbd706641874a37021fd64af36419a82ac9
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-11-18 14:14:53 -0800

    CLEANED CODE

--- getenv.c
+++ getenv.c
@@ -3,7 +3,7 @@
 /**
  * get_environ - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *        constant function prototype.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
