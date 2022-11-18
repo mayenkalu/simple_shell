1fd0ca6de1df250f1fa00b113a4019467c8defde
Mayen Kalu <kalumayen@gmail.com>
2022-11-18 01:28:26 -0800

    Simple shell project by Mayen Kalu and Chesachi Victor Kalu

--- getenv.c
+++ getenv.c
@@ -1,45 +1,93 @@
-#include "main.h"
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "shell.h"
 
 /**
- * _getenv - gets the value of an environment variable
- * @var: name of environment variable
- *
- * Return: malloc'd string containing value of the variable
- * or NULL on error and if the environment variable doesn't exist
-*/
-char *_getenv(char *var)
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+char **get_environ(info_t *info)
 {
-	int i = 0, len;
-	char *str, **env;
-
-	env = __environ;
-
-	if (!var)
-		return (NULL);
-
-	while (env[i])
+	if (!info->environ || info->env_changed)
 	{
-		len = _strlen(var);
-
-		/* the environment varible exists */
-		if (_strncmp(var, env[i], len) == 0)
-		{
-			/*
-			* Copy variable value to new malloc'd memory,
-			* copy only after the = sign e.g PATH=WIE -> WIE
-			 */
-			str = _strdup(&env[i][len + 1]);
-
-			if (!str)
-				return (NULL);
-
-			return (str);
-		}
-		i++;
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
 	}
 
-	return (NULL);
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *           or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ * Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+																						}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
 }
--- main.c
+++ main.c
@@ -1,120 +1,43 @@
-#include "main.h"
-#include <unistd.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-
-void fork_n_execute(char **args, char *env[], char *argv[], int *status);
-void handle_sigint(int sig);
+#include "shell.h"
 
 /**
  * main - entry point
- * @argc: numbers of command line arguments
- * @argv: array of command line arguments
- * @env: environment variables
- *
- * Return: Always 0
-*/
-int main(__attribute__((unused)) int argc, char *argv[], char *env[])
+ * @ac: arg count
+ * @av: arg vector
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-	char *input = NULL, *cmd;
-	char **args;
-	int status = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	signal(SIGINT, handle_sigint);
-	while (*(input = prompt()))
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		args = split(input);
-		free(input);
-
-
-		/* Skip current execution if the no command was passed */
-		if (!args)
-			continue;
-
-		/* Handle replacements */
-		replace_variables(args, status);
-
-		/* Handle built in commands */
-		if (handle_builtins(args, argv[0]))
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			free_split(args);
-			continue;
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-
-		/* Get full command path e.g ls -> /bin/ls */
-		cmd = get_command(args[0]);
-
-		if (!cmd)
-		{
-			/* If command doesn't exist skip the current execute */
-			_puts("Error\n");
-			free_split(args);
-			continue;
-		}
-
-		/* Free previous command before overwriting it*/
-		free(args[0]);
-		args[0] = cmd;
-
-		/* Create a child process and execute the command in the child process*/
-		fork_n_execute(args, env, argv, &status);
-
-		/* Free all args */
-		free_split(args);
+		info->readfd = fd;
 	}
-
-	free(input);
-	return (0);
-}
-
-/**
- * fork_n_execute - creates a child process of the main process and executes a
- * command in it
- * @args: An array containing the command and the command arguments where
- * args[0] is the command itself
- * @env: An array of the environment variables
- * @argv: The array arguments passed into the executing program
- * @status: an integer pointer where the status of the
- * executed process is stored
- *
- * Return: void
-*/
-void fork_n_execute(char **args, char *env[], char *argv[], int *status)
-{
-	pid_t child;
-
-	child = fork();
-
-	/* Execute command only if the current process is the child process*/
-	if (child == 0)
-	{
-		if (execve(args[0], args, env) == -1)
-		{
-			perror(argv[0]);
-			exit(1);
-		}
-	}
-	else
-	{
-		/* Wait for child process to exit before continuing*/
-		wait(status);
-	}
-
-}
-
-/**
- * handle_sigint - handler for signal
- * @sig: signal
- *
- * Return: void
-*/
-void handle_sigint(int sig)
-{
-	(void) sig;
-
-	write(STDERR_FILENO, "\n$", 2);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
