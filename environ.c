79e1ccf653c65583b4105f36d1fb873a3b6fbc22
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-11-18 14:07:50 -0800

    environ code adjusted

--- environ.c
+++ environ.c
@@ -3,7 +3,7 @@
 /**
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *        constant function prototype.
  * Return: Always 0
  */
 int _myenv(info_t *info)
